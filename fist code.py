99658daad8d6d88e269caa39e5e4b96530dbb1f5
Phat301207 <buithanhphat9a3@gmail.com>
2024-06-18 10:54:56 +0700

    Add files via upload

---
+++ fist code.py
@@ -0,0 +1,8 @@
+#Tinh delta
+print("Pt bậc 2 có dạng:ax^2+bx+c=0")
+a=int(input("Nhập a: "))
+b=int(input("nhập b: "))
+c=int(input("nhập c: "))
+delta=b**2-4*a*c
+if delta < 0:
+    print("phương trình vô nghiệm")
